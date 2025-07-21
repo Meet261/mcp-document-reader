580867fedfb3b493ce4de1d4397c8896e5406b00
Meet261 <meetkumarmangroliya5@gmail.com>
2025-07-21 14:27:30 +0200

    improved the server configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,7 @@
-// vite.config.js
-export default {
-    base: "./",
-  };
-  
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+});
